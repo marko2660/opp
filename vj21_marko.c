10c3b0b82e3398f57d75f11cb0e14b0c9f429acc
marko2660 <marko.rnjak04@gmail.com>
2025-10-20 10:12:07 +0200

    Add files via upload

---
+++ vj21_marko.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int* fibonacci(int n) {
+	if (n <= 0) return NULL;
+
+	int* niz = (int*)malloc(n * sizeof(int));
+	if (niz == NULL) {
+		printf("Greska pri alokaciji memorije!\n");
+		return NULL;
+	}
+
+	niz[0] = 1;
+	if (n > 1)
+		niz[1] = 1;
+
+	for (int i = 2; i < n; i++) {
+		niz[i] = niz[i - 1] + niz[i - 2];
+	}
+
+	return niz;
+}
+
+int main() {
+	int n;
+
+	printf("Unesite broj elemenata Fibonacci niza: ");
+	scanf("%d", &n);
+
+	int* niz = fibonacci(n);
+
+	if (niz != NULL) {
+		printf("Fibonacci niz:\n");
+		for (int i = 0; i < n; i++) {
+			printf("%d ", niz[i]);
+		}
+		printf("\n");
+
+		free(niz);
+	}
+
+	return 0;
+}
---
+++ vj22.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int* data;       
+	int size;         
+	int capacity;       
+} Vector;
+void vector_delete(Vector* v) {
+	free(v->data);
+	v->data = NULL;
+	v->size = 0;
+	v->capacity = 0;
+}
+void vector_delete(Vector* v) {
+	free(v->data);
+	v->data = NULL;
+	v->size = 0;
+	v->capacity = 0;
+}
+void vector_push_back(Vector* v, int value) {
+	if (v->size >= v->capacity) {
+		v->capacity *= 2;
+		v->data = (int*)realloc(v->data, v->capacity * sizeof(int));
+		if (v->data == NULL) {
+			printf("Realloc error!\n");
+			exit(1);
+		}
+	}
+	v->data[v->size++] = value;
+}
+void vector_pop_back(Vector* v) {
+	if (v->size > 0) {
+		v->size--;
+	}
+	else {
+		printf("Vector is already empty!\n");
+	}
+}
+int vector_front(Vector* v) {
+	if (v->size > 0) {
+		return v->data[0];
+	}
+	else {
+		printf("Vector is empty!\n");
+		return -1; 
+	}
+}
+int vector_back(Vector* v) {
+	if (v->size > 0) {
+		return v->data[v->size - 1];
+	}
+	else {
+		printf("Vector is empty!\n");
+		return -1;
+	}
+}
+int vector_size(Vector* v) {
+	return v->size;
+}
+int main() {
+	Vector v = vector_new(2);
+
+	vector_push_back(&v, 10);
+	vector_push_back(&v, 20);
+	vector_push_back(&v, 30); 
+
+	printf("Front: %d\n", vector_front(&v));
+	printf("Back: %d\n", vector_back(&v));
+	printf("Size: %d\n", vector_size(&v));
+
+	vector_pop_back(&v);
+	printf("After pop_back, size: %d\n", vector_size(&v));
+
+	vector_delete(&v);
+	return 0;
+}
+
